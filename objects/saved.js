77c428605ad6392fc23f09bf4de0668633691af1
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2024-04-23 13:52:25 +0200

    Add saving profile index

--- objects/saved.js
+++ objects/saved.js
@@ -1,6 +1,30 @@
 class Saved {
     skippedFromSaving = ["skippedFromSaving"]
 
+    NewExpirationDate() {
+        let expirationDate = new Date();
+        expirationDate.setMonth(expirationDate.getMonth()+1);
+        return expirationDate;
+    }
+
+    IndexProfile(newProfile) {
+        let profiles = this.GetAllProfiles();
+
+        if (profiles.find((profile) => profile == newProfile) == null) {
+            console.log("Added new profile: " + newProfile);
+            profiles.push(newProfile);
+        }
+        Cookie.set("profiles", profiles.toString(), this.NewExpirationDate());
+    }
+
+    GetAllProfiles() {
+        let profiles = Cookie.get("profiles").split(',');
+        if (profiles[0] == '{')
+            profiles = [];
+
+        return profiles;
+    }
+
     Load(profile = "default") {
         let cookie = JSON.parse(Cookie.get(profile));
         for(const element in cookie) {
@@ -9,6 +33,7 @@ class Saved {
     }
     
     Save(profile = "default") {
+        this.IndexProfile(profile);
         let out = "{"
         let index = 0;
         for(const element in this) {
@@ -19,9 +44,6 @@ class Saved {
         out = out.substring(0, out.length - 1);
         out += "\n}";
         
-        let expirationDate = new Date();
-        expirationDate.setMonth(expirationDate.getMonth()+1);
-
-        Cookie.set(profile, out, expirationDate);
+        Cookie.set(profile, out, this.NewExpirationDate());
     }
 }
